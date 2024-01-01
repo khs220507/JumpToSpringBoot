521464db1a4349af05b681c8725e90056ec1b775
khs220507 <khs220507@gmail.com>
2024-01-01 17:10:38 +0900

    lombok

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,15 +1,16 @@
-package com.example.demo;
+package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-	@GetMapping("hello")
+public class MainController {
+	@GetMapping("/sbb")
 	@ResponseBody
-	public String hello() {
-		return "Hello World";
+	public String index() {
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
 	}
+	
 
 }
